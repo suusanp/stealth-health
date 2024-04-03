62a50f1049aa50b2438c2d30974ea568e2dea47d
Susan Pan <144385544+suusanp@users.noreply.github.com>
2024-04-03 15:07:10 -0400

    Update metricsUtils.js

--- metricsCalculation/metricsUtils.js
+++ metricsCalculation/metricsUtils.js
@@ -1,14 +1,16 @@
-// Description: This file contains the function to compute available functionalities based on enabled metrics.
+// Description: This file contains the function to return available functionalities based on enabled metrics.
 
 const computeAvailableFunctionalities = (metrics) => {
-  const availableFunctionalities = ['BMI',]; // Always available
+  const availableFunctionalities = ['Body Mass Index (BMI)', 'Basal Metabolic Rate (BMR)']; // Always available from User Profile
 
   // Check each metric and add corresponding functionalities
+  // This is not accurate yet and should be replaced with actual logic. 
+
   if (metrics.dailySteps) {
-    availableFunctionalities.push('Trend Analysis', 'Calorie Burn Estimation');
+    availableFunctionalities.push('Calorie Burn Estimation');
   }
   if (metrics.heartRate) {
-    availableFunctionalities.push('Calorie Burn Estimation', 'Sleep Quality Assessment', 'Heart Rate Zones for Exercise');
+    availableFunctionalities.push('Heart Rate Zones for Exercise');
   }
   if (metrics.bloodPressure) {
     availableFunctionalities.push('Blood Pressure');
