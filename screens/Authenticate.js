e54aa065b882498d052faf09a8ed7b3129b942bb
Jeffrey Kauv Li <jeffrey.kauvli@mail.mcgill.ca>
2024-04-03 13:51:19 -0400

    setup two screens for authentication

---
+++ screens/Authenticate.js
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet, Switch, Alert } from 'react-native';
+
+const Authenticate = () => {
+
+
+    return(
+        <View></View>
+    );
+
+};
+
+const styles = StyleSheet.create({
+
+
+
+});
+
+export default Authenticate;
