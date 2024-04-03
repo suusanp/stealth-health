80cc41dde91366afbe450d194ff426cd5319c51c
Jeffrey Kauv Li <jeffrey.kauvli@mail.mcgill.ca>
2024-04-03 13:51:19 -0400

    setup two screens for authentication

---
+++ screens/SetupAuth.js
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet, Switch, Alert } from 'react-native';
+
+const SetupAuth = () => {
+
+
+    return(
+        <View></View>
+    );
+
+};
+
+const styles = StyleSheet.create({
+
+
+
+});
+
+export default SetupAuth;
