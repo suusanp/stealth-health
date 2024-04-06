cd2f2491818e83833cb3b5c3eb3841e51c87fda5
Susan Pan <144385544+suusanp@users.noreply.github.com>
2024-04-06 18:45:39 -0400

    data deletion notification scheduler done
    working (2024)

--- services/PushNotificationManager.js
+++ services/PushNotificationManager.js
@@ -14,30 +14,14 @@ Notifications.setNotificationHandler({
 
 export async function PushNotificationManager(title, body) {
     try {
-        //console.log('PushNotificationManager: Start');
         const expoPushToken = await registerForPushNotificationsAsync();
-        //console.log('PushNotificationManager: Expo Push Token:', expoPushToken);
-
         await schedulePushNotification(title, body);
-        console.log('PushNotificationManager: Notification sent successfully');
-
         return 'Success';
     } catch (error) {
         console.error('PushNotificationManager Error:', error);
         return 'Error';
     }
 }
-
-// export async function testNoti() {
-//     try {
-//         console.log('testNoti: Start');
-//         const result = await PushNotificationManager('Test Title', 'Test Body');
-//         console.log('testNoti: Result:', result);
-//     } catch (error) {
-//         console.error('testNoti Error:', error);
-//     }
-// }
-
 
 async function schedulePushNotification(title, body) {
     await Notifications.scheduleNotificationAsync({
---
+++ services/ScheduleNotifications.js
@@ -0,0 +1,63 @@
+// getDataRetentionPeriod.js
+import { PushNotificationManager } from "./PushNotificationManager";
+import { getPreferences } from '../backend/FileSystemService'; // Adjust the import path as per your project structure
+
+async function getUserDataRetentionPeriod() {
+    try {
+        const preferences = await getPreferences();
+        const dataRetentionPeriod = preferences.dataRetention; // Default to '1 Month' if not available
+        console.log('User Data Retention Period:', dataRetentionPeriod);
+        // convert dataRetentionPeriod to seconds
+        let seconds = 0;
+        if (dataRetentionPeriod === '5 Seconds') {
+            seconds = 3603;
+        }
+        if (dataRetentionPeriod === '1 Day') {
+            seconds = 24 * 60 * 60;
+        } else
+        if (dataRetentionPeriod === '3 Days') {
+            seconds = 3 * 24 * 60 * 60;
+        } else
+        if (dataRetentionPeriod === '1 Week') {
+            seconds = 7 * 24 * 60 * 60;
+        } else if (dataRetentionPeriod === '1 Month') {
+            seconds = 30 * 24 * 60 * 60;
+        } else if (dataRetentionPeriod === '3 Months') {
+            seconds = 3 * 30 * 24 * 60 * 60;
+        } else if (dataRetentionPeriod === '6 Months') {
+            seconds = 6 * 30 * 24 * 60 * 60;
+        } else if (dataRetentionPeriod === '1 Year') {
+            seconds = 365 * 24 * 60 * 60;
+        }
+        seconds = seconds * 1000; // convert seconds to milliseconds, as setTimeout uses milliseconds
+
+        // send a notification to the user an hour before the data retention period is reached
+        warning_seconds = seconds - 3600 * 1000;
+        const warning_title = 'Data Retention Period Reached';
+        const warning_body = 'Your data will be deleted in an hour. If you want to keep it, please take action now!';
+
+        setTimeout(() => {
+            PushNotificationManager(warning_title, warning_body);
+        }, warning_seconds);
+
+        console.log('Time until warning:', warning_seconds);
+        // send a notification to the user when the data retention period is reached
+        const title = 'Data Retention Period Reached';
+        const body = 'Your data has been deleted.';
+
+        setTimeout(() => {
+            PushNotificationManager(title, body);
+        }, seconds);
+
+        console.log('Time until deletion:', seconds);
+
+    } catch (error) {
+        console.error('Error fetching Data Retention Period:', error);
+    }
+}
+
+export default getUserDataRetentionPeriod;
+
+
+
+
