e3051d325a181b760f7c1f6e4e406dfdcbb99067
Susan Pan <144385544+suusanp@users.noreply.github.com>
2024-04-07 21:11:19 -0400

    Update PrivacyPolicySimplified.js

--- screens/privacyPolicies/PrivacyPolicySimplified.js
+++ screens/privacyPolicies/PrivacyPolicySimplified.js
@@ -6,25 +6,25 @@ const PrivacyPolicySimplified = () => {
         <View style={styles.container}>
             <Text style={styles.appName}>Privacy Policy</Text>
             <Text style={styles.policyText}>
-                <Text style={styles.heading}>Information Collection:</Text> We only keep the information you type into our app on your own device. We don't collect anything else or share your data. Our app doesn’t send your data over the internet.
+                <Text style={styles.heading}>Information Collection:</Text> We only keep the information you type into our app on your own device. We don't collect anything else or share your data. Our app doesn’t send your data over the internet. 
             </Text>
             <Text style={styles.policyText}>
-                <Text style={styles.heading}>Data Usage:</Text> Your input is used for health calculations within the app, all processed locally. Your sensitive health info is encrypted for security.
+                <Text style={styles.heading}>Data Usage:</Text> Your input is used for health calculations within the app, all processed locally. Your sensitive health info is encrypted for security. 
             </Text>
             <Text style={styles.policyText}>
-                <Text style={styles.heading}>Data Sharing:</Text> Your info is never shared outside your device; it stays private.
+                <Text style={styles.heading}>Data Sharing:</Text> Your info is never shared outside your device; it stays private. Even we can’t see it.
             </Text>
             <Text style={styles.policyText}>
                 <Text style={styles.heading}>Your Control:</Text> You decide how long your data stays (3 days to 1 year), can delete your account/data anytime, and choose what health data to share for accurate metrics.
             </Text>
             <Text style={styles.policyText}>
-                <Text style={styles.heading}>Data Retention:</Text> You can choose how long the app keeps your data, from days to a year. Deleting your account is easy too.
+                <Text style={styles.heading}>Data Retention:</Text> You can choose how long your data stays on your device, and we will remind you when your Retention Period arrives. After that, it's automatically deleted. You can also delete your account and all data anytime.
             </Text>
             <Text style={styles.policyText}>
                 <Text style={styles.heading}>Analytics & Ads:</Text> The app doesn’t use analytics or show ads, keeping your data private from third parties.
             </Text>
             <Text style={styles.policyText}>
-                <Text style={styles.heading}>Security Measures:</Text> Health data on your device is encrypted. You can also add extra security with Face ID or a phone password.
+                <Text style={styles.heading}>Security Measures:</Text> Health data on your device is encrypted. The information is scrambled into a format that's unreadable to prevent unauthorized access. You can also add extra security with Face ID or a phone password. 
             </Text>
             <Text style={styles.policyText}>
                 <Text style={styles.heading}>Policy Changes:</Text> If the policy changes, you’ll be notified and asked for consent again.
